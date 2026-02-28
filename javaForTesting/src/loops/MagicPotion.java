e4a45acbe55978b6b56c69e82c230feb44c76115
jayas <jayasharma14nov@gmail.com>
2026-02-28 14:38:44 -0800

    simple FOR loop

---
+++ javaForTesting/src/loops/MagicPotion.java
@@ -0,0 +1,26 @@
+package loops;
+
+public class MagicPotion {
+
+	static int i;
+
+	public static void main(String[] args) {
+
+	MagicPotion magic = new MagicPotion();
+	magic.makePotion();
+
+	}
+
+	public void makePotion() {
+
+		for (i = 1; i < 5; i++) {
+
+			System.out.println("Magic ingredient  " + i + " added VOILAAAAAA");
+
+		}
+
+		System.out.println("MAGIC POTION IS READY!!!!");
+
+	}
+
+}
