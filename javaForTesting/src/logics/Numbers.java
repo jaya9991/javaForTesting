84fe6fdc89997d4ca0b12f0131528d48c744bce5
jayas <jayasharma14nov@gmail.com>
2026-02-28 14:39:59 -0800

    numbers IFELSE

---
+++ javaForTesting/src/logics/Numbers.java
@@ -0,0 +1,29 @@
+package logics;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+
+		int b = 0;
+
+	//	System.out.println(b);
+
+		if (a > b) {
+
+			System.out.println("a is greater than b");
+		}
+
+		else {
+
+			System.out.println("b is greater than a");
+
+		}  
+
+		
+	}
+
+
+		
+}
