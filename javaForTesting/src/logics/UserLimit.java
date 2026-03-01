4ddad4896b6cfb2581a55ee862a5e4c66bd42e4d
jayas <jayasharma14nov@gmail.com>
2026-03-01 14:41:59 -0800

    edited

--- javaForTesting/src/logics/UserLimit.java
+++ javaForTesting/src/logics/UserLimit.java
@@ -7,62 +7,83 @@ public class UserLimit {
 		// transaction (any amount) ---- approval from user1 max 100-----approval from
 		// user2 max 100------approved
 
-		int transaction = 350;
-		int user1Limit = 100;
-		// int user2Limit= user1Limit;
-		
-		System.out.println("transation initiated for " + transaction);
-		if (transaction <= user1Limit) {
+		int money = 400;
+		int limit = 100; // should be made static as it never changes
 
-			System.out.println("user1 approved the transaction of " + transaction);
+		UserLimit user = new UserLimit();
+		user.getResult(money, limit);
 
-			user1Limit -= transaction;
-
-			System.out.println("user2 approved the trancation of " + transaction);
-
-			System.out.println("Left daily limit of user1 and user2 is  " + user1Limit);
-
-		} else if (transaction > user1Limit) { 
-
-			System.out.println("On day 1 user1 approved  " + user1Limit);
-			int pending = transaction - user1Limit;
-
-			System.out.println("Amount pending to be approved on day 2 is " + pending);
-
-			do {
-
-				if (pending <= user1Limit) {
-
-					System.out.println("user1 approved the pending amount of " + pending);
-
-					user1Limit -= pending;
-
-				} else if (pending > user1Limit) {
-
-					// System.out.println("Amount too large push to another day");
-					System.out.println("On day 2 user1 approved another  " + user1Limit);
-
-					int dayNextamount = pending - user1Limit;
-
-					System.out.println("Amount pending to be approved on next day is " + dayNextamount);
-				//	System.out.println("Pending amount of " + dayNextamount + " was approved");
-
-				}
-
-			} while (pending < 100);
-
-			/*
-			 * if (day2amount <= user1Limit) {
-			 * 
-			 * System.out.println("user1 approved the pending amount of " + day2amount);
-			 * 
-			 * user1Limit -= day2amount;
-			 * 
-			 * } else if(day2amount > user1Limit) {
-			 * 
-			 * System.out.println("Amount too large push to another day"); } }
-			 */
-		}
 	}
 
+	public void getResult(int transaction, int userLimit) { // parametrize things that can change
+		// int transaction = 150;
+		// int user1Limit = 100;
+		// int user2Limit= user1Limit;
+		System.out.println("transation initiated for:   " + transaction);
+
+		if (transaction <= userLimit) {
+
+			System.out.println("user1 approved the transaction of:   " + transaction);
+
+			userLimit -= transaction;
+
+			System.out.println("user2 approved the transaction of:   " + transaction);
+
+			System.out.println("Daily limit left of user1 and user2 is:    " + userLimit);
+
+		} else if (transaction > userLimit) {
+
+			System.out.println("On day 1 user1 approved:    " + userLimit);
+
+			int pending = transaction - userLimit; // store the remaining amount that needs to be approved the next day
+
+			System.out.println("Amount pending to be approved on day 2 is:   " + pending);
+
+			  do {
+
+				if (pending <= userLimit) {
+
+					System.out.println("user1 approved the pending amount of:   " + pending);
+
+					userLimit -= pending;
+
+				} else if (pending > userLimit) {
+
+					// System.out.println("Amount too large push to another day");
+					System.out.println("On day 2 user1 approved another:  " + userLimit);
+
+					int dayNextamount = pending - userLimit;
+
+					System.out.println("Amount pending to be approved on next day is:   " + dayNextamount);
+					// System.out.println("Pending amount of " + dayNextamount + " was approved");
+                   
+						
+				} 
+			  }
+				while (pending < userLimit);
+						 
+			  
+		}
+	}
 }
+
+
+				/*
+				 * if (day2amount <= user1Limit) {
+				 * 
+				 * System.out.println("user1 approved the pending amount of " + day2amount);
+				 * 
+				 * user1Limit -= day2amount;
+				 * 
+				 * } else if(day2amount > user1Limit) {
+				 * 
+				 * System.out.println("Amount too large push to another day"); } }
+				 */
+			
+			  
+		
+	
+	
+		  
+			
+			 
