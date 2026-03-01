859badccc97ec8672152ac7e9360114c7fedfc60
jayas <jayasharma14nov@gmail.com>
2026-03-01 15:13:40 -0800

    nested FOR

---
+++ javaForTesting/src/loops/StarPattern.java
@@ -0,0 +1,23 @@
+package loops;
+
+public class StarPattern {
+
+	public static void main(String[] args) {
+	
+		for(int i=0; i<5; i++  ) {
+			
+			for (int a=0; a<=i; a++) {
+				
+					System.out.print("*");	
+				
+			}
+			
+			System.out.println(" ");
+			
+		}
+		
+	     
+
+	}
+
+}
