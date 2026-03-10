86421f6942af6a927b472549894b6862325ff940
jayas <jayasharma14nov@gmail.com>
2026-03-09 20:56:44 -0700

    09/03

---
+++ javaForTesting/src/logics/StringLength.java
@@ -0,0 +1,28 @@
+package logics;
+
+public class StringLength {
+	
+	//String is a class and has many methods in it which can be called on variables using dot operators
+	
+
+	public static void main(String[] args) {
+		
+		String name = "PASSWORD";
+		
+		int display = name.length();
+		
+		System.out.println(display);
+		
+		
+		String user = " ACCEPTED";
+		
+		name = name.concat(user);
+		
+		System.out.println(name);
+		
+		
+		
+
+	}
+
+}
