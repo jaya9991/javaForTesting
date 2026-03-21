fa5563de443f9b4a81106a92bd73ad4f3ff6d6d3
jayas <jayasharma14nov@gmail.com>
2026-03-20 22:15:48 -0700

    new 2

---
+++ javaForTesting/src/inheritance/Dog.java
@@ -0,0 +1,25 @@
+package inheritance;
+
+public class Dog extends Animal{
+	
+	String breed;
+	
+	void sound() {
+		
+		System.out.println("dog makes sound");
+		
+	}
+
+
+	void eat() {
+		
+		System.out.println("dog eats");
+	}
+
+	void bark() {
+		
+		System.out.println("dog barks");
+	}
+	
+
+}
