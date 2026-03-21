c902bdf6ede0baa985bd79059c7bcf34b40086c7
jayas <jayasharma14nov@gmail.com>
2026-03-20 22:15:03 -0700

    20/26

---
+++ javaForTesting/src/inheritance/Animal.java
@@ -0,0 +1,28 @@
+package inheritance;
+
+public class Animal {
+	
+		int age;
+		String name;
+		
+		
+		void sound() {
+			
+			System.out.println("animal makes sound");
+			
+		}
+	
+	
+		void eat() {
+			
+			System.out.println("animal eats");
+		}
+	
+	
+	
+		void age() {
+			
+			System.out.println("animal's age");
+		}
+
+}
