07e9ea9f82af639ade4b65819f251f4940a0a516
jayas <jayasharma14nov@gmail.com>
2026-03-20 22:15:25 -0700

    new

---
+++ javaForTesting/src/inheritance/Cat.java
@@ -0,0 +1,25 @@
+package inheritance;
+
+public class Cat extends Animal{
+
+	
+	String catBreed;
+	
+	
+	void sound() {
+		
+		System.out.println("cat makes sound");
+		
+	}
+
+
+	void eat() {
+		
+		System.out.println("cat eats");
+	}
+
+	
+	
+	
+}
+
